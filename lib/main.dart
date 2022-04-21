b79579ff0ca18df4c07731759c1812f1f5e93d9a
Danielle Kom <daniellekom1@gmail.com>
2022-04-21 12:05:57 -0400

    added more design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -19,14 +19,14 @@ class IdCard extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:  [
+          children: [
             Center(
               child: CircleAvatar(
-              backgroundImage: AssetImage('assets/head_shot.png'),
-              radius: 60.0,
+                backgroundImage: AssetImage('assets/head_shot.png'),
+                radius: 60.0,
               ),
             ),
-            SizedBox(height:40.0),
+            SizedBox(height: 40.0),
             Text(
               "NAME",
               style: TextStyle(
@@ -77,34 +77,43 @@ class IdCard extends StatelessWidget {
                   fontSize: 28.0,
                   fontWeight: FontWeight.bold),
             ),
-             SizedBox(height: 30.0),
-               Text(
+            SizedBox(height: 30.0),
+            Text(
               "CONTACT",
               style: TextStyle(
                   color: Colors.grey,
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.bold),
             ),
-              SizedBox( height:5.0),
-             Row(
-             children: [
-               Icon(
-               Icons.email,
-               color: Colors.grey[400], 
-               ),
-               SizedBox(width: 10.0),
-              Text(
-                'Danikom@gmail.com',
-                style:TextStyle(
+            SizedBox(height: 5.0),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
                   color: Colors.grey[400],
-                  fontSize: 18.0,
-                  letterSpacing: 1.0,
-                )
-              )
-             ],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'Danikom@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('you clicked me');
+        },
+        child: Icon(Icons.add_rounded),
+        backgroundColor: Colors.black,
+        foregroundColor: Color.fromARGB(255, 248, 248, 248),
+        splashColor: Colors.orange,
       ),
     );
   }
