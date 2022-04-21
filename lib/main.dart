9730c514107a75eaf5e54cd6caaf26663900253c
Danielle Kom <daniellekom1@gmail.com>
2022-04-21 11:07:37 -0400

    id card

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,111 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-home: IdCard()
-));
+void main() => runApp(MaterialApp(home: IdCard()));
 
 class IdCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text('Verification ID Card')
+        title: const Text('Verification ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+            Center(
+              child: CircleAvatar(
+              backgroundImage: AssetImage('assets/head_shot.png'),
+              radius: 60.0,
+              ),
+            ),
+            SizedBox(height:40.0),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "Danielle Kom",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 217, 184, 68),
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "AGE",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "30",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 217, 184, 68),
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "LOCATION",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "Fort Lauderdale, Fl",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 217, 184, 68),
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+             SizedBox(height: 30.0),
+               Text(
+              "CONTACT",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold),
+            ),
+              SizedBox( height:5.0),
+             Row(
+             children: [
+               Icon(
+               Icons.email,
+               color: Colors.grey[400], 
+               ),
+               SizedBox(width: 10.0),
+              Text(
+                'Danikom@gmail.com',
+                style:TextStyle(
+                  color: Colors.grey[400],
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                )
+              )
+             ],
+            ),
+          ],
         ),
-      
+      ),
     );
   }
 }
